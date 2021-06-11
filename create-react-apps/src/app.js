7dfbedd4a6e47946b3e2ac2869a9a8793a8c1198
egoTr <luongvannghia@gmail.com>
2021-06-11 19:50:49 +0700

    Another shit

--- create-react-apps/src/app.js
+++ create-react-apps/src/app.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import ToDo from './todo-v2/todo';
+import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import ReactColors from './react-colors/_main';
 
-export default function App() {
-  return (
-    <ToDo />
-  ) // return
-} // App
+class App extends Component {
+  static defaultProps = {
+  } // default properties
+
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+    } // state
+
+    // this.methodName2 = this.methodName2.bind(this); // bind 'this' from a class-inside method
+    // this.methodName3 = methodName3.bind(this); // bind 'this' from a class-outside method
+
+  } // constructor
+
+  // an EXPERIMENTAL approach to bind 'this'
+  methodName = () => {
+  } // end of method
+
+  render() {
+     return (
+       <BrowserRouter>
+          <ReactColors />
+      </BrowserRouter>
+     ) // return
+  } // render
+} // end of class
+
+export default App;
